84a531d348391a87ba703e876a71b0cbff268d28
cybertex99 <cybertex99@gmail.com>
2026-03-05 20:24:09 +0100

    Update app.py

--- arezzo-sicura-project/app.py
+++ arezzo-sicura-project/app.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template, jsonify
-import os
+from flask import Flask, render_template, os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# Endpoint di test per verificare che il server risponda
-@app.route('/api/updates')
-def get_updates():
-    return jsonify({"status": "online"})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
