307343a589af709bc5a8c36637d50c5fc9b70196
cybertex99 <cybertex99@gmail.com>
2026-03-06 16:44:22 +0100

    Update app.py

--- arezzo-sicura-project/app.py
+++ arezzo-sicura-project/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, os
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    # Usa la porta fornita da Render o la 5000 come default
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
